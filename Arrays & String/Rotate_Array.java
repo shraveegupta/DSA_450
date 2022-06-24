3827f290a3b02f5c7e865ae20a02d5e6d9a5f4d3
shravee gupta <61349418+shraveegupta@users.noreply.github.com>
2022-06-24 17:23:13 +0530

    Update Rotate_Array.java

--- Arrays & String/Rotate_Array.java
+++ Arrays & String/Rotate_Array.java
@@ -23,3 +23,17 @@ Constraints:
 0 <= k <= 105
 
 */
+
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int[] temp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            temp[(i + k) % nums.length] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = temp[i];
+        }
+        return;
+    }
+}
