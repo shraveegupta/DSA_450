e6fe47201ad05d0bdf966c9d096e1eef16b76208
shravee gupta <61349418+shraveegupta@users.noreply.github.com>
2022-06-30 23:16:02 +0530

    Update Count_Prime.java

--- Arrays & String/Count_Prime.java
+++ Arrays & String/Count_Prime.java
@@ -19,3 +19,36 @@ Constraints:
 0 <= n <= 5 * 106
 
 */
+
+class Solution
+{
+    public int countPrimes(int n)
+    {
+        if(n <= 2)
+        {
+            return 0;
+        }
+        boolean [] composites = new boolean[n];
+        int limit = (int)Math.sqrt(n);
+        for(int i = 2; i <= limit; i++)
+        {
+            if(composites[i] == false)
+            {
+                for(int j = i*i; j < n; j += i)
+                {
+                    composites[j] = true;
+                }
+            }
+        }
+    
+        int count = 0;
+        for(int i = 2; i < n; i++)
+        {
+            if(composites[i] == false)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
